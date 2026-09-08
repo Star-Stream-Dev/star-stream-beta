925ea87c81fba67bad034c81278110bfdb9e933e
Rebelleader <daniel.liddel@icloud.com>
2026-09-08 15:50:58 -0400

    Update RetroArchPlayer.tsx

--- src/components/RetroArchPlayer.tsx
+++ src/components/RetroArchPlayer.tsx
@@ -70,9 +70,9 @@ export function RetroArchPlayer({ onClose }: RetroArchPlayerProps) {
           <EmulatorJS
             EJS_core={selectedCore as any}
             EJS_gameUrl={romUrl}
-            EJS_pathtodata="https://cdn.emulatorjs.org/stable/data/"
+            EJS_pathtodata="https://cdn.emulatorjs.org/latest/data/"
             EJS_startOnLoaded={true}
-            EJS_threads = true
+            EJS_threads ={true}
           />
         </div>
       </div>
