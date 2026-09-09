6cc354d2a4d85a29a507bba76c69f760c830ea5a
Rebelleader <daniel.liddel@icloud.com>
2026-09-09 11:51:59 -0400

    Update RetroArchPlayer.tsx

--- src/components/RetroArchPlayer.tsx
+++ src/components/RetroArchPlayer.tsx
@@ -73,6 +73,7 @@ export function RetroArchPlayer({ onClose }: RetroArchPlayerProps) {
             EJS_pathtodata="https://cdn.emulatorjs.org/latest/data/"
             EJS_startOnLoaded={true}
             EJS_threads ={true}
+            EJS_disableBatchBootup={true}
           />
         </div>
       </div>
